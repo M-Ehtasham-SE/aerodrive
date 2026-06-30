b6e426b64cdd557e4c672e7be9f5e530efdfea8b
M-Ehtasham-SE <ehtashamsd@gmail.com>
2026-06-30 16:01:42 +0500

    fix: add ssl to db connection for TiDB

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,11 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      }
+    },
     logging: false,
     pool: { max: 10, min: 0, acquire: 30000, idle: 10000 }
   }
